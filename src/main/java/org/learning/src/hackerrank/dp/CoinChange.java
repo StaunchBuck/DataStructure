17794ee0010903ecc33b165bca618b9a7885affe
subhasish halder <subhasish.halder09@gmail.com>
2025-05-22 17:13:51 +0530

    First Commit

---
+++ src/main/java/org/learning/src/hackerrank/dp/CoinChange.java
@@ -0,0 +1,31 @@
+package org.learning.src.hackerrank.dp;
+
+import java.util.Collections;
+import java.util.List;
+
+public class CoinChange {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static long getWays(int n, List<Integer> c) {
+		Integer[] sums = new Integer[n+1];
+	      sums[0]=1;
+	      Collections.sort(c);
+	      for(Integer coin: c){
+	        for(Integer sum=0;sum<=n;sum++){
+	          if(coin<=sum){
+	            sums[sum] +=sums[sum-coin];
+	          }
+	        } 
+	      }
+	      return sums[n];
+	}
+	
+	public void finalize() {
+		
+	}
+
+}
